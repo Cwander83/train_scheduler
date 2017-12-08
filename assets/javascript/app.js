95ad8c511fad9da3bda7892af1c5a25b63645f5a
Cwander83 <chriswandermail@gmail.com>
2017-12-08 14:26:02 -0500

    12-8-17(working on JS)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,43 @@
-console.log("work");
+//========firebase startup
+console.log("conectedtoJS");
+var config = {
+    apiKey: "AIzaSyDYjXKopvYZolIlNad3rX2jCBSbCYx_k8o",
+    authDomain: "train-scheduler-d49cb.firebaseapp.com",
+    databaseURL: "https://train-scheduler-d49cb.firebaseio.com",
+    projectId: "train-scheduler-d49cb",
+    storageBucket: "train-scheduler-d49cb.appspot.com",
+    messagingSenderId: "48676926096"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  $("#submit").on("click", function (event) {
+    event.preventDefault();
+    var name = $("#train-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var firstTrainTime = $("#firsttraintime-input").val().trim();
+    var frequency = $("#frequency-input").val().trim();
+    console.log(name);
+    console.log(destination);
+    console.log(firstTrainTime);
+    console.log(frequency);
+
+var newTrain = {
+    name: name,
+    destination: destination,
+    firsttraintime: firstTrainTime,
+    frequency: frequency
+
+};
+
+database.ref().push(newTrain);
+
+$("#traintable").append("<tr><td>"+name+"</td><td>"+destination+"</td><td>"+firstTrainTime+"</td><td>"+frequency+"</td></tr>")
+
+    $("#name-input").val("");
+    $("#destination-input").val("");
+    $("#firsttraintime-input").val("");
+    $("#frequency-input").val("");
+
+  });
