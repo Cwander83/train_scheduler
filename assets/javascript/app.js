c38f087ce62d9ff18f4d743c22a8ffdb4855ea6f
Cwander83 <chriswandermail@gmail.com>
2017-12-09 16:35:06 -0500

    12-9-17(working on times)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 //========firebase startup
-console.log("conectedtoJS");
+//console.log("conectedtoJS");
 var config = {
     apiKey: "AIzaSyDYjXKopvYZolIlNad3rX2jCBSbCYx_k8o",
     authDomain: "train-scheduler-d49cb.firebaseapp.com",
@@ -7,37 +7,55 @@ var config = {
     projectId: "train-scheduler-d49cb",
     storageBucket: "train-scheduler-d49cb.appspot.com",
     messagingSenderId: "48676926096"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  $("#submit").on("click", function (event) {
+$("#submit").on("click", function (event) {
     event.preventDefault();
     var name = $("#train-input").val().trim();
     var destination = $("#destination-input").val().trim();
     var firstTrainTime = $("#firsttraintime-input").val().trim();
     var frequency = $("#frequency-input").val().trim();
-    console.log(name);
-    console.log(destination);
-    console.log(firstTrainTime);
-    console.log(frequency);
+    //console.log(name);
+    //console.log(destination);
+    //console.log(firstTrainTime);
+    //console.log(frequency);
 
-var newTrain = {
-    name: name,
-    destination: destination,
-    firsttraintime: firstTrainTime,
-    frequency: frequency
+    var newTrain = {
+        name: name,
+        destination: destination,
+        firsttraintime: firstTrainTime,
+        frequency: frequency
 
-};
+    };
 
-database.ref().push(newTrain);
+    database.ref().push(newTrain);
 
-$("#traintable").append("<tr><td>"+name+"</td><td>"+destination+"</td><td>"+firstTrainTime+"</td><td>"+frequency+"</td></tr>")
+    $("#traintable").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + firstTrainTime + "</td><td>" + frequency + "</td></tr>")
 
-    $("#name-input").val("");
+    $("#train-input").val("");
     $("#destination-input").val("");
     $("#firsttraintime-input").val("");
     $("#frequency-input").val("");
 
-  });
+});
+
+database.ref().on("child_added", function (childSnapShot, prevChildKey) {
+
+    var cs = childSnapShot.val();
+
+    //console.log(childSnapShot.val());
+
+    var nameFire = (cs.name);
+    var destFire = (cs.role);
+    var firstFire = (cs.date);
+    var frequencyFire = (cs.rate);
+
+    //var epmStartPretty = moment.unix(dateFire).format("MM/DD/YYYY")
+    //console.log(epmStartPretty);
+
+    $("#employeeList").append("<tr><td>" + nameFire + "</td><td>" + destFire + "</td><td>" + firstFire + "</td><td>" + frequencyFire + "</td></tr>")
+
+})
